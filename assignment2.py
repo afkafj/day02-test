c142a093b80f9bedee810009ce29ff9c72aeabed
K <kk0412368@gmail.com>
2023-01-13 17:34:33 +0900

    S

--- assignment2.py
+++ assignment2.py
@@ -4,10 +4,10 @@ secret = random.randint(1,10)
 guess = int(input('숫자를 입력해보세요: '))
 
 if guess > secret:
-    pass
+    print("too high")
 
 if guess < secret:
-    pass
+    print("too low")
 
 if guess == secret:
-    pass
+    print("just right")
