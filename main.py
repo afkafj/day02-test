1fcb25c371847e661696f8311b6d25e560105f47
K <kk0412368@gmail.com>
2023-01-13 17:51:34 +0900

    t

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+green = True
+small = False
+
+if green:
+    print("이건 완두콩이고 초록색이야 ")
+else:
+    print("이건 호박이고 초록색이 아냐")
+
+if small:
+    print("이건 체리고 작아")
+else:
+    print("이건 수박이고 작지 않아")
+
